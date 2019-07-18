30e88e345bfa5590cc6e148d3b8b818429dd9b63
subodhss23@gmail.com <subodhss23@gmail.com>
2019-07-17 20:57:04 -0700

    helment added

--- express201/helmetAndOthers.js
+++ express201/helmetAndOthers.js
@@ -1,7 +1,10 @@
 
 const express = require('express');
+const helment = require('helmet');
+
 const app = express();
- 
+
+app.use(helmet());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
