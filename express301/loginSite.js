c1b33ee840ed8e2978500a1f6b26a544cbc9858c
subodhss23@gmail.com <subodhss23@gmail.com>
2019-07-19 15:01:06 -0700

    adding req.params with id

--- express301/loginSite.js
+++ express301/loginSite.js
@@ -6,18 +6,35 @@ const app = express();
 const helmet = require('helmet');
 app.use(helmet())
 
+const cookieParser = require('cookie-parser');
+
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use( function(req, res, next){
+  if(req.query.msg === 'fail'){
+      res.locals.msg = 'Sorry, username or the password does not match';
+  } else {
+    res.locals.msg = ''
+  }
+  next();
+});
+
 app.get('/', function(req, res, next){
+  console.log(req.param())
   res.send('Sanity check');
 })
 
 app.get('/login', function(req, res, next){
+  // the req object has a query  property in express
+  // req.query is an object,with a property of every key in the query string
+  // the query string is where you put insecure data
+  // cannot be password, username
   res.render('login');
 })
 
@@ -36,8 +53,11 @@ app.post('/process_login', function(req, res, next){
         res.cookie('username', username);
         //res.redirect takes 1 args: where to send user
         res.redirect('/welcome');
+      }  else {
+        //the ? is a specical character in url
+        //it indicates end of URL resource path and start of query parameters.
+        res.redirect('/login?msg=fail&test=hello');
       }
-
   
   // req.body is made by urlencoded., which pareses
   // the http message for sent data
@@ -45,7 +65,31 @@ app.post('/process_login', function(req, res, next){
 })
 
 app.get('/welcome', function(req, res, next){
-  res.send('<h1>Welcome, you have successfully logged in.</h1>')
+  //req.cookies object will have a property for every named
+  // cookie that has been set.
+  res.render('welcome', {
+    username: req.cookies.username
+  });
+})
+
+/**is a router, anytime somethings has a : in front it is a wildcard!
+ * wildcard, will match anything in the slot
+ */
+app.get('/story/:storyId', function(req, res, next){
+  /** the req.params object always exists,
+   * it will have a property for each wildcar in the route
+   */
+  res.send(`<h1>Story ${req.params.storyId}</h1>`);
+  console.log(req.params.storyId);
+
+})
+
+app.get('/logout', function(req, res, next){
+  //res.clearCookie takes one args
+  //1. cookie to clear (by name)
+  //clearing data/cookie from their local machine
+  res.clearCookie('username')
+  res.redirect('/login');
 })
 
 app.listen(3000, function(){
